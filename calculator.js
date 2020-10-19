b4fa7e0eda1110f41bfcf30a19b7a3ec4879556a
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2020-10-19 21:01:45 +0300

    Solved Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 // Define the numbers and the operation here
-
+let firstNumber = 10
+let secondNumber = 2
+let operation = "/"
 // Write if-statements here
+if (operation === "+"){
+    console.log (`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)}
+else if (operation === "-"){
+    console.log (`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`)}
+else if (operation === "*"){ 
+    console.log (`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`)}
+else if (operation === "/"){
+    console.log (`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
+    
+}
